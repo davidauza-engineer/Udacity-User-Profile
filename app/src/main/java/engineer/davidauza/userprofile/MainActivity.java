7ab1088b38161e2268de0161b5a41cd7d4f22fb6
davidauza.engineer <davidauza01@gmail.com>
2019-03-30 16:24:14 -0500

    Initial commit

---
+++ app/src/main/java/engineer/davidauza/userprofile/MainActivity.java
@@ -0,0 +1,13 @@
+package engineer.davidauza.userprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
