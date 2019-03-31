26d0185686d26c3f970d0703ac20e9a9de3501f4
davidauza.engineer <davidauza01@gmail.com>
2019-03-30 20:30:28 -0500

    Layout Done.

--- app/src/main/java/engineer/davidauza/userprofile/MainActivity.java
+++ app/src/main/java/engineer/davidauza/userprofile/MainActivity.java
@@ -1,7 +1,10 @@
 package engineer.davidauza.userprofile;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the profile picture of the user
+        ImageView userImageView = findViewById(R.id.profile_picture);
+        userImageView.setImageResource(R.drawable.profile_picture);
+
+        // Set the name of the user
+        TextView nameTextView = findViewById(R.id.name);
+        nameTextView.setText(R.string.main_name);
+
+        // Set the birthday of the user
+        TextView birthdayTextView = findViewById(R.id.birthday);
+        birthdayTextView.setText(R.string.main_birthday);
+
+        // Set the country of the user
+        TextView countryTextView = findViewById(R.id.country);
+        countryTextView.setText(R.string.main_country);
     }
 }
